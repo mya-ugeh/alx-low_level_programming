24f674f3873c0dd2fcb361437b3c140da4fdb108
mya-ugeh <ugehmya@yahoo.com>
2023-08-11 15:02:50 -0700

    script added

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  * Description: possible combination of single numbers
+  *
+  * Return: always 0
+  */
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+		if (a == '9')
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
