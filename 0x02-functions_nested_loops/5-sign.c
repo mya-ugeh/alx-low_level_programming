f1852a6ff3dd2206a3a524ea22b5b1ffb7c5b4e2
mya-ugeh <ugehmya@yahoo.com>
2023-08-15 17:35:00 -0700

    script added

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * print_sign - Prints the sign of a number.
  * @n: Number to be checked
@@ -9,19 +8,19 @@
  */
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        _putchar('+');
-	return (1);
-    }
-    else if (n == 0)
-    {
-        _putchar(48);
-	return (0);
-    }
-    else
-    {
-        _putchar('-');
-    }
-    return (-1);
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+	}
+	return (-1);
 }
