a39b072e942e860fcd3a218990c0790b24d16f75
mya-ugeh <ugehmya@yahoo.com>
2023-08-16 16:50:55 -0700

    script added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * add -  two integers and returns the result.
+  * @a: first integer
+  * @b: second integer
+  *
+  * Return: result
+  */
+int add(int a, int b)
+{
+	return (a + b);
+}
