be2fde16c7fa3cd1c484585d9c9ecac1e56296a7
mya-ugeh <ugehmya@yahoo.com>
2023-08-18 08:26:53 -0700

    script added

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * more_numbers - prints 10 times the numbers, from 0 to 14
+  * Return: void
+  */
+void more_numbers(void)
+{
+	int i;
+
+	int j;
+
+	j = 0;
+	while (j < 10)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+			}
+			else
+				_putchar(i + '0');
+		}
+		_putchar('\n');
+		j++;
+	}
+}
