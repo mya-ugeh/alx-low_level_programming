737119992129ac1e799da4ef94e09ae7d8e6ae8e
mya-ugeh <ugehmya@yahoo.com>
2023-08-26 02:06:17 -0700

    script added

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * print_times_table - print n times table
+  * @n: number to be printed
+  *
+  * Return: void
+  */
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	if ( n > 15 || n < 0)
+		return;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			k = i * j;
+			if (j == 0)
+				_putchar((k % 10) +'0');
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (k >= 100)
+				{
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else if (k >= 10)
+				{
+						_putchar((k / 10) + '0');
+				}
+		
+				_putchar((k % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
