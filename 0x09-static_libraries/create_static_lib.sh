5f65505bc0ab20a6897ae5499ece7ae1fa5d50ed
mya-ugeh <ugehmya@yahoo.com>
2023-09-01 07:07:40 -0700

    script added

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-ar -rc liball.a *.o
+
+# Compile all .c files in the current directory and create an object file for each
+gcc -c *.c
+
+# Create a static library named liball.a from all the object files
+ar rcs liball.a *.o
+
+# Clean up the temporary object files
+rm -f *.o
