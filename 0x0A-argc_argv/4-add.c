4ae87df2665df1767f3173103898d99233888bd0
mya-ugeh <ugehmya@yahoo.com>
2023-09-01 14:12:33 -0700

    script added

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+  * main - entry code
+  * @argc: number of parameter
+  * @argv: array of strings
+  * Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	int i, j,  sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+	else
+		printf("0\n");
+	return (0);
+}
