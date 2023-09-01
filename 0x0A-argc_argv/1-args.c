30c076cee80dd33bb29fa98b837b30cb8ddea9f0
mya-ugeh <ugehmya@yahoo.com>
2023-09-01 12:51:45 -0700

    script added

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * main - check the code
+  *
+  * argc: number of argument
+  * argv: print string
+  * Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
