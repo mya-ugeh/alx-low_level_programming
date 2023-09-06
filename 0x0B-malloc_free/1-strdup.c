936b3f27873fc988c959ee21637c40f807adf21a
mya-ugeh <ugehmya@yahoo.com>
2023-09-06 15:08:09 -0700

    script added

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strdup - Duplicate a string.
+ * @str: The input string to duplicate.
+ *
+ * Return: A pointer to the duplicated string, or NULL on failure.
+ */
+char *_strdup(char *str)
+{
+	size_t len;
+	char *duplicate;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	len = strlen(str);
+
+	duplicate = (char *)malloc(len);
+	if (duplicate == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(duplicate, str);
+
+	return (duplicate);
+}
