09f33a5bc99a788ec11de285deb25e788d145a70
mya-ugeh <ugehmya@yahoo.com>
2023-09-13 10:02:14 -0700

    script added

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+  * new_dog - creates new dog
+  * @name: name of new dog
+  * @age: age of new dog
+  * @owner: owner of new dog
+  * Return: dog
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog = (dog_t *)malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+
+	new_dog->name = strdup(name);
+	new_dog->owner = strdup(owner);
+
+	if (new_dog->name == NULL || new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
+		return (NULL);
+	}
+
+	new_dog->age = age;
+	return (new_dog);
+}
