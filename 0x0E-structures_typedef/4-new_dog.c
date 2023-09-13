e699998d80bee0e77444fd703c48960e7d372c01
mya-ugeh <ugehmya@yahoo.com>
2023-09-13 10:05:53 -0700

    script added

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,7 +22,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->name = strdup(name);
 	new_dog->owner = strdup(owner);
 
-	if (new_dog->name == NULL || new_dog->owner == NULL)
+	if (new_dog->name == NULL && new_dog->owner == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog->owner);
