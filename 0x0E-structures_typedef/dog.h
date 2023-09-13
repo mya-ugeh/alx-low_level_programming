5f7c1c1f8df8c9da5e04663f29f4ed82a2ea6e8d
mya-ugeh <ugehmya@yahoo.com>
2023-09-13 08:42:07 -0700

    script added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Represents information about a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: This struct stores information about a dog, including its name, age, and owner.
+ */
+
 struct dog
 {
 	char *name;
