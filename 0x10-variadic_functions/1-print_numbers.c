ad4e4dd341efffc7a9b772936ceb4f24fa60f7e8
mya-ugeh <ugehmya@yahoo.com>
2023-09-15 06:09:53 -0700

    script added

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,22 +10,18 @@
   */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
-
 	va_list list;
-
-	if (separator == NULL)
-		return;
+	unsigned int i;
 
 	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(list, int));
-		if (i < n - 1)
+		if ((i < n - 1) & (separator != NULL))
 			printf("%s", separator);
 	}
 
-	va_end(list);
 	printf("\n");
+	va_end(list);
 }
