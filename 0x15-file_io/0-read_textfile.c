611c2f8d7c2992525fa2ada5d85c48738ec0b6d9
mya-ugeh <ugehmya@yahoo.com>
2023-10-17 07:27:16 -0700

    script added

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,51 @@
 #include <stdio.h>
-#include <fcntl.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <stddef.h>
-#include "main.h"
-/**
-  * read_textfile - reads a text file and prints it to POSIZ stdout
-  * @filename: the name of the file
-  * @letters: the number of letters that should be read and printed
-  * Return: actual number of letters
-  */
 
+/**
+ * read_textfile - Reads a text file and prints it to standard output.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed. 0 on failure.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	char buffer[1024];
-	ssize_t bytes_read, tots_read, bytes_written;
-
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	int file_descriptor;
+	ssize_t bytes_read, bytes_written;
+	char *buffer;
+
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
 		return (0);
 
-	tots_read = 0;
-
-	while (letters > 0)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 	{
-		bytes_read = read(fd, buffer, sizeof(buffer));
-		if (bytes_read <= 0)
-			break;
-
-		bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
-		if (bytes_written != bytes_read)
-		{
-			close (fd);
-			return(0);
-		}
-		tots_read += bytes_read;
-		letters -= bytes_read;
+		close(file_descriptor);
+		return (0);
 	}
 
-	close (fd);
-	return (tots_read);
+	bytes_read = read(file_descriptor, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
+
+	free(buffer);
+	close(file_descriptor);
+	return (bytes_read);
 }
